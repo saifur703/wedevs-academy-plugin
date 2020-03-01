050c1af06f86961a353b05c627e3971a6194e246
saifur <saifur703@gmail.com>
2020-03-01 22:14:58 +0600

    create plugin file and add the plugin header

---
+++ wedevs-academy-plugin.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * Plugin Name: weDevs Academy
+ * Description: A tutorial plugin for weDevs Academy
+ * Plugin URI: https://tareq.co
+ * Author: Tareq Hasan
+ * Author URI: https://tareq.co
+ * Version: 1.0
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if( ! defined("ABSPATH") ){
+    exit;
+}
